dc1125e0767ca5e341e20c18748582ccd20f4f1b
Jay Namburi <nambjay@Mac.attlocal.net>
2025-03-12 12:38:23 -0500

    Update Next.js config for Netlify deployment

--- next.config.js
+++ next.config.js
@@ -1,9 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   images: {
-    domains: ['images.unsplash.com'],
     unoptimized: true,
   },
+  trailingSlash: true,
 };
 
 module.exports = nextConfig;
